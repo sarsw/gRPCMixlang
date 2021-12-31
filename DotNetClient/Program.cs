3a9194bc998a4ef60c0a00bf9e282d23e4e1418e
Steve Russell <steve.russell@swintontechnology.com>
2021-12-31 12:32:48 +0000

    Added c# console client to connect and make use of the c++ server and common helloworld.proto file

---
+++ DotNetClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Grpc.Core;
+using Helloworld;       // from the proto file generated cc file namespace
+
+namespace DotNetClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+            String user = "Steve";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+        }
+    }
+}
